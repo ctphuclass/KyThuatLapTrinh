e8e01134acec5876128a6ed2f5b99b87d18bb514
ctphu <phu.t.cao@outlook.com>
2019-10-12 16:24:32 +0700

    Ham - Chuong trinh con

--- BaiTapHam/BaiTapHam/Program.cs
+++ BaiTapHam/BaiTapHam/Program.cs
@@ -19,17 +19,70 @@ namespace BaiTapHam
 	{
 		public static void Main(string[] args)
 		{
+			BaiTapChiaHet();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		static void BaiTapChiaHet()
+		{
+			int iN;
+			Console.WriteLine("Bai Tap Chia Het!");
+			iN = InputInt("Nhap N", 2, 1000);
+			Console.WriteLine("N = {0}", iN);
+			
+			if(CoLaSoNguyenTo(iN) == true)
+			{
+				Console.WriteLine("{0} La so nguyen to", iN);
+			}
+			else
+			{
+				Console.WriteLine("{0} khong phai la so nguyen to", iN);
+			}
+			
+		}
+		
+		static bool CoLaSoNguyenTo(int piN)
+		{
+			bool bResult = false;
+			for (int i = 2; i <= piN - 1 ; i++) 
+			{
+				if(piN % i == 0)
+				{
+					bResult = false;
+					return bResult;
+				}
+			}
+			bResult = true;
+			return bResult;
+			
+		}
+		
+		static bool ChiaHetCho3(int piN)
+		{
+			bool bResult = false;
+			if(piN % 3 == 0)
+			{
+				bResult = true;
+			}
+			else
+			{
+				bResult = false;
+			}
+			return bResult;
+		}
+		
+		static void BaiTapVeHinh()
+		{
 			int iN, iM;
 			Console.WriteLine("Ve Hinh Chu Nhat!");
 			
 			iN = InputInt("Nhap N", 3, 10);
+			
 			iM = InputInt("Nhap M", 3, 10);
+			
 			DrawRectangle(iN, iM);
-			
-			
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
 		}
 		
 		static int InputInt(string psMessage, int piMin, int piMax)
