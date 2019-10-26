344165921e077ff00e3751225aa9b2528ea074ee
ctphu <phu.t.cao@outlook.com>
2019-10-26 13:52:12 +0700

    Tham Bien Tham Tri

--- BaiTapHam/BaiTapHam/Program.cs
+++ BaiTapHam/BaiTapHam/Program.cs
@@ -19,16 +19,63 @@ namespace BaiTapHam
 	{
 		public static void Main(string[] args)
 		{
-			BaiTapChiaHet();
-			
+			//BaiTapSoNguyenTo();
+			BaiTapGiaiPhuongTrinh();
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
 		
-		static void BaiTapChiaHet()
+		static void BaiTapGiaiPhuongTrinh()
+		{
+			Console.WriteLine("Giai phuong trinh bac 2");
+			double dA,dB,dC,dX1,dX2;
+			int iSoNghiem;
+			dA = InputDouble("Nhap A", 1, 1000);
+			dB = InputDouble("Nhap B", 1, 1000);
+			dC = InputDouble("Nhap C", 1, 1000);
+			iSoNghiem = GPTB2(dA, dB, dC, out dX1, out dX2);
+			if(iSoNghiem == 0)
+			{
+				Console.WriteLine("Phuong trinh {0}*X*X + {1}*X + {2} = 0 Vo nghiem",dA,dB,dC);
+			}
+			else if(iSoNghiem == 1)
+			{
+				Console.WriteLine("Phuong trinh {0}*X*X + {1}*X + {2} = 0 Co 1 Nghiem X={3}",dA,dB,dC,dX1);
+			}
+			else
+			{
+				Console.WriteLine("Phuong trinh {0}*X*X + {1}*X + {2} = 0 Co 2 Nghiem X1={3}, X2={4}",dA,dB,dC,dX1, dX2);
+			}
+		}
+		
+		static int GPTB2(double pdA, double pdB, double pdC, out double X1, out double X2)
+		{
+			int iResult = 0;
+			X1 = 0;
+			X2 = 0;
+			double delta = (pdB * pdB) - (4 * pdA * pdC);
+			if (delta < 0) 
+			{
+				iResult = 0;	
+			}else if (delta == 0) 
+			{
+				iResult = 1;
+				X1 = (0-pdB)/(2*pdA);
+				X2 = (0-pdB)/(2*pdA);
+			}
+			else
+			{
+				iResult = 2;
+				X1 = (0-pdB + Math.Sqrt(delta) )/(2*pdA);
+				X2 = (0-pdB - Math.Sqrt(delta) )/(2*pdA);
+			}
+			return iResult;
+		}
+		
+		static void BaiTapSoNguyenTo()
 		{
 			int iN;
-			Console.WriteLine("Bai Tap Chia Het!");
+			Console.WriteLine("Bai Tap So Nguyen To!");
 			iN = InputInt("Nhap N", 2, 1000);
 			Console.WriteLine("N = {0}", iN);
 			
@@ -55,6 +102,7 @@ namespace BaiTapHam
 				}
 			}
 			bResult = true;
+			
 			return bResult;
 			
 		}
@@ -93,6 +141,16 @@ namespace BaiTapHam
 				iResult = int.Parse(Console.ReadLine());
 			}while (iResult < piMin || iResult > piMax);
 			return iResult;
+		}
+		
+		static double InputDouble(string psMessage, double piMin, double piMax)
+		{
+			double dResult = 0;
+			do{
+				Console.Write("{0} [{1},{2}]: ", psMessage, piMin, piMax);
+				dResult = double.Parse(Console.ReadLine());
+			}while (dResult < piMin || dResult > piMax);
+			return dResult;
 		}
 		
 		static void DrawRectangle(int piN, int piM)
